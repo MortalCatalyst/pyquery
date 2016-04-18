4478cef947d6e16273dcef3bf602f048ee89b8d4
mortalcatalyst <flebber.crue@gmail.com>
2016-04-19 09:23:11 +1000

    did it

--- jqxml.py
+++ jqxml.py
@@ -1,16 +1,33 @@
 from pyquery import PyQuery as pq
 import pandas as pd
+import os
+import sys
 
+if len(sys.argv) == 2:
+    print("no params")
+    sys.exit(1)
 
-d = pq(filename='20160319RHIL0_edit.xml')
+dir = sys.argv[1]
+mask = sys.argv[2]
 
-# from nominations
-res = d('nomination')
-nomID = [res.eq(i).attr('id') for i in range(len(res))]
-horseName = [res.eq(i).attr('horse') for i in range(len(res))]
+files = os.listdir(dir)
 
-attrs = ('id', 'horse')
-data = [[res.eq(i).attr(x) for x in attrs] for i in range(len(res))]
+fileResult = filter(lambda x: x.endswith(mask), files)
+
+# d = pq(filename='20160319RHIL0_edit.xml')
+
+for items in fileResult:
+    d = pq(filename=items)
+    res = d('nomination')
+    attrs = ('id', 'horse')
+    data = [[res.eq(i).attr(x) for x in attrs] for i in range(len(res))]
+
+    # from nominations
+# res = d('nomination')
+# nomID = [res.eq(i).attr('id') for i in range(len(res))]
+# horseName = [res.eq(i).attr('horse') for i in range(len(res))]
+
+# attrs = ('id', 'horse')
 
 frames = pd.DataFrame(data)
 print(frames)
