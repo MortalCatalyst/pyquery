72c78a7aee4ace6acc09eca53f725fb75706e808
mortalcatalyst <flebber.crue@gmail.com>
2016-04-21 23:37:02 +1000

    up to multi columns

--- jqxml.py
+++ jqxml.py
@@ -15,22 +15,53 @@ def GetArgs(parser):
     return parser.parse_args()
 
 fileList = GetArgs(parser)
-print(fileList.files)
+# print(fileList.files)
 
 data = []
-frames = pd.DataFrame([])
 
-attrs = ('id', 'horse')
+
+horseattrs = ('id', 'horse', 'number', 'finished', 'age', 'sex', 'blinkers',
+              'trainernumber', 'career', 'thistrack', 'firstup',
+              'secondup', 'variedweight', 'weight', 'pricestarting')
+meetattrs = ('id', 'venue', 'date', 'rail', 'weather', 'trackcondition')
+raceattrs = ('id', 'number', 'shortname', 'stage', 'distance', 'grade',
+             'age', 'weightcondition', 'fastesttime', 'sectionaltime')
+clubattrs = ('code')
+
+frames = pd.DataFrame([])
 
 for items in fileList.files:
     d = pq(filename=items)
+    meet = d('meeting')
+    club = d('club')
+    race = d('race')
     res = d('nomination')
-    dataSets = [[res.eq(i).attr(x)
-                 for x in attrs] for i in range(len(res))]
-    print(dataSets)
-    aDF = pd.DataFrame(dataSets)
-    frames = frames.append(aDF)
+    resdata = [[res.eq(i).attr(x)
+                for x in horseattrs] for i in range(len(res))]
+    # print(dataSets)
+    meetdata = [[meet.eq(i).attr(x)
+                 for x in meetattrs] for i in range(len(meet))]
+    racedata = [[race.eq(i).attr(x)
+                 for x in raceattrs] for i in range(len(race))]
+    clubdata = [[club.eq(i).attr(x)
+                 for x in clubattrs] for i in range(len(club))]
+    raceid = [row[0] for row in racedata]
+    # L = [x + [0] for x in L]
+    print(resdata)
+    # resdata = [raceid[i] for i in raceid  x + i for x in resdata]
 
+    # for number of classes equalling nomination in the each category of
+    # race inset raceid into resdata
+    #
+    print(resdata)
+    clubdf = pd.DataFrame(clubdata)
+    meetdf = pd.DataFrame(meetdata)
+    racedf = pd.DataFrame(racedata)
+    resdf = pd.DataFrame(resdata)
+    frames = frames.append(clubdf)
+    frames = frames.append(meetdf)
+    frames = frames.append(racedf)
+    frames = frames.append(resdf)
 
-print(frames)
-frames.to_csv('~/testingFrame.csv', encoding='utf-8')
+# print(frames)
+# frames.to_csv('~/testingFrame3.csv', encoding='utf-8')
